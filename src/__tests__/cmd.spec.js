079fb4ceb95bf86c5343d6b0d5c46a586c979470
Rockie Guo <rockie@kitmi.com.au>
2021-09-24 16:23:05 +1000

    fix: run command live stdout bug

--- src/__tests__/cmd.spec.js
+++ src/__tests__/cmd.spec.js
@@ -5,4 +5,12 @@ describe('unit:cmd', function() {
         const code = await runLive_('ls', ['-l']);
         code.should.be.exactly(0);       
     });
+
+    it('cmd live 2',  async function () {
+        await runLive_('ls', ['-l'], (data) => {
+            console.log('stdout', data.toString());
+        }, (data) => {
+            console.error('stderr', data.toString());
+        });
+    });
 });
